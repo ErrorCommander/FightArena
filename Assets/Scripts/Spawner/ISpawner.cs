6f261d51225593c452a094fa2657021f4cee31e1
ErrorCommander <kai_412@mail.ru>
2022-07-06 10:09:36 +0300

    Released SpawOnClick

--- Assets/Scripts/Spawner/ISpawner.cs
+++ Assets/Scripts/Spawner/ISpawner.cs
@@ -2,5 +2,10 @@
 
 public interface ISpawner
 {
+    /// <summary>
+    /// Spawn copy of the prefeb Fighter
+    /// </summary>
+    /// <param name="prefab">Refeb Fighter for spawn</param>
+    /// <returns>Transform created Fighter</returns>
     public Unit SpawnUnit(Unit prefab);
 }
